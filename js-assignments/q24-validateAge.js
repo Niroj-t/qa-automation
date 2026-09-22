ac0bf907899ecf92fc8217c8e32d783d1642da29
Niroj Thapa <you@niroj.com>
2026-09-22 12:32:41 +0545

    feat(part2): add custom ValidationError class with validateAge

---
+++ js-assignments/q24-validateAge.js
@@ -0,0 +1,20 @@
+class ValidationError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = 'ValidationError';
+  }
+}
+
+function validateAge(age) {
+  if (age < 0) throw new ValidationError('Age must be positive');
+  return age;
+}
+
+console.log(validateAge(25));
+
+try {
+  validateAge(-3);
+} catch (err) {
+  console.log(err.name);    
+  console.log(err.message); 
+}
